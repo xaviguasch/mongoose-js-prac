d2910d7344470baa37b5f889eb685be3b65f9166
xaviguasch <xaviguasch@gmail.com>
2022-07-05 21:06:35 +0200

    created schema and model for movie

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ mongoose
     console.log('Oh no error!!!')
     console.log(error)
   })
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+const amadeus = new Movie({
+  title: 'Amadeus',
+  years: 1986,
+  score: 9.2,
+  rating: 'R',
+})
